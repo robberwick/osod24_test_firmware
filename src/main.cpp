e223a264980ba13727e8c18e65933709c37de56b
Rob Berwick <rob.berwick@gmail.com>
2023-11-26 15:58:01 +0000

    Add Receiver and Navigator libraries
    Added `navigator` and `receiver` directories in the `libs` directory by updating the `CMakeLists.txt` in `libs`. In `main.cpp`, added instances of Receiver and Navigator classes and `navigate` method in while loop. Updated `CMakeLists.txt` to link these directories to the main project. These changes are necessary for introducing navigation functionalities based on receiver's data.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
+#include <cstdio>
 #include "pico/stdlib.h"
+#include "navigator.h"
+#include "receiver.h"
+#include "motor2040.hpp"
 
 
 int main() {
     stdio_init_all();
+
+    Receiver receiver = Receiver(motor::motor2040::RX_ECHO);
+    Navigator navigator = Navigator(receiver);
+
+    while (true) {
+        navigator.navigate();
+        sleep_ms(20);
+    }
 }
