e8571fba78419549b625531b1bd65b7a47cbc549
Rob Berwick <rob.berwick@gmail.com>
2023-12-04 20:23:57 +0000

    Implement interrupt-driven navigation mechanism
    Added a hardware timer to trigger navigation and adjusted scheduling accordingly. In the new code structure, an interrupt is triggered every 20 ms, setting a flag that signals the navigation function to execute on the next loop iteration of the main function. This approach is designed to improve responsiveness, reduce latency, and free up CPU cycles.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include "pico/stdlib.h"
+#include "pico/time.h"
 #include "navigator.h"
 #include "receiver.h"
 #include "statemanager.h"
@@ -7,28 +8,48 @@
 #include "motor2040.hpp"
 #include "tank_steer_strategy.h"
 
+Navigator *navigator;
+bool shouldNavigate = false;
+
+extern "C" void timer_callback(repeating_timer_t *t) {
+    // Call the navigate function in the interrupt handler
+    shouldNavigate = true;
+}
 
 int main() {
     stdio_init_all();
 
     // set up the state estimator
-    auto* pStateEstimator = new STATE_ESTIMATOR::StateEstimator();
+    auto *pStateEstimator = new STATE_ESTIMATOR::StateEstimator();
 
     // set up the state manager
     using namespace STATEMANAGER;
-    auto* pTankSteerStrategy = new MIXER::TankSteerStrategy();
-    auto* pStateManager = new StateManager(pTankSteerStrategy);
+    auto *pTankSteerStrategy = new MIXER::TankSteerStrategy();
+    auto *pStateManager = new StateManager(pTankSteerStrategy);
 
     // set up the receiver
     // if the cmake build flag RX_PROTOCOL is CPPM, then use the CPPM receiver
     // otherwise use the SBUS receiver
-    Receiver* pReceiver = getReceiver(motor::motor2040::RX_ECHO);
+    Receiver *pReceiver = getReceiver(motor::motor2040::RX_ECHO);
 
     // set up the navigator
-    Navigator navigator = Navigator(pReceiver, pStateManager);
+    navigator = new Navigator(pReceiver, pStateManager);
+
+    // Initialize a hardware timer
+    repeating_timer_t navigationTimer;
+    add_repeating_timer_ms(
+            20,
+            reinterpret_cast<repeating_timer_callback_t>(timer_callback),
+            nullptr,
+            &navigationTimer
+    );
 
     while (true) {
-        navigator.navigate();
-        sleep_ms(20);
+        // Do nothing in the main loop
+        if (shouldNavigate) {
+            // Call the navigate function in the interrupt handler
+            navigator->navigate();
+            shouldNavigate = false;
+        }
     }
 }
