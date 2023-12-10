c5962dfb47fa604ad513f3bb827d4d156380ca16
markmellors <mark.mellors@gmail.com>
2023-12-10 15:36:21 +0000

    first draft of a state estimating function, based only on encoders

--- libs/state_estimator/src/state_estimator.cpp
+++ libs/state_estimator/src/state_estimator.cpp
@@ -19,6 +19,19 @@ namespace STATE_ESTIMATOR {
         encoders.REAR_LEFT->init();
         encoders.REAR_RIGHT->init();
 
+        // Initialize the State struct member variables
+        estimatedState.x = 0.0f;
+        estimatedState.xdot = 0.0f;
+        estimatedState.y = 0.0f;
+        estimatedState.ydot = 0.0f;
+        estimatedState.velocity = 0.0f;
+        estimatedState.heading = 0.0f;
+        estimatedState.angularVelocity = 0.0f;
+        estimatedState.FL_wheel_speed = 0.0f;
+        estimatedState.FR_wheel_speed = 0.0f;
+        estimatedState.RL_wheel_speed = 0.0f;
+        estimatedState.RR_wheel_speed = 0.0f;
+        
         instancePtr = this;
         setupTimer();
     }
@@ -33,6 +46,59 @@ namespace STATE_ESTIMATOR {
 
     void StateEstimator::publishState() const {
         showValues();
+    }
+
+    void StateEstimator::estimateState() {
+        // calculate position deltas
+        float distance_travelled;
+        float heading_change;
+        float left_travel;
+        float right_travel;
+        //get average wheel rotation for left side
+        left_travel = ((encoders.FRONT_LEFT->radians()) + (encoders.REAR_LEFT->radians())) / 2;
+        // convert wheel rotation to distance travelled in meters
+        left_travel = left_travel * CONFIG::WHEEL_DIAMETER / 2;
+        right_travel = ((encoders.FRONT_RIGHT->radians()) + (encoders.REAR_RIGHT->radians())) / 2;
+        right_travel = right_travel * CONFIG::WHEEL_DIAMETER / 2;
+
+        distance_travelled = (left_travel - right_travel) / 2;
+        heading_change = (left_travel + right_travel) / CONFIG::WHEEL_TRACK;
+
+        //calculate new position and orientation
+        //calc a temp heading halfway between old heading and new
+        //assumed to be representative of heading during distance_travelled
+        float tempHeading;
+        tempHeading = estimatedState.heading + heading_change / 2;
+        estimatedState.x = estimatedState.x + distance_travelled * sin(estimatedState.heading);
+        estimatedState.y = estimatedState.y + distance_travelled * cos(estimatedState.heading);
+
+        //now actually update heading
+        estimatedState.heading = estimatedState.heading + heading_change;
+
+        //calculate speeds
+        float left_speed;
+        float right_speed;
+        
+        //get wheel speeds
+        estimatedState.FL_wheel_speed = encoders.FRONT_LEFT->radians_per_second();
+        estimatedState.FR_wheel_speed = encoders.FRONT_RIGHT->radians_per_second();
+        estimatedState.RL_wheel_speed = encoders.REAR_LEFT->radians_per_second();
+        estimatedState.RR_wheel_speed = encoders.REAR_RIGHT->radians_per_second();
+        left_speed = (estimatedState.FL_wheel_speed + estimatedState.RL_wheel_speed) / 2;
+        
+        // convert wheel rotation to distance travelled in meters
+        left_speed = left_speed * CONFIG::WHEEL_DIAMETER / 2;
+
+        //repeat for right side
+        right_speed = (estimatedState.FR_wheel_speed + estimatedState.RR_wheel_speed) / 2;
+        right_speed = right_speed * CONFIG::WHEEL_DIAMETER / 2;
+
+        //calc all velocities
+        estimatedState.velocity = (left_speed - right_speed) / 2;
+        estimatedState.xdot = estimatedState.velocity * sin(estimatedState.heading);
+        estimatedState.ydot = estimatedState.velocity * cos(estimatedState.heading);
+        estimatedState.angularVelocity= (left_speed + right_speed) / CONFIG::WHEEL_TRACK;
+
     }
 
     void StateEstimator::setupTimer() {
