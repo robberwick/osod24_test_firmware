50c5f3d8a9c94d601ba22b0e8a8e75a380992869
markmellors <mark.mellors@gmail.com>
2023-12-10 11:52:33 +0000

    arduino example copied verbatim, no mods yet to work on rp2040

--- src/main.cpp
+++ src/main.cpp
@@ -7,24 +7,48 @@
 #include "tank_steer_strategy.h"
 
 
+#include <Wire.h>
+#include "SparkFun_BNO080_Arduino_Library.h" // Click here to get the library: http://librarymanager/All#SparkFun_BNO080
+
+BNO080 myIMU;
+
 int main() {
-    stdio_init_all();
+  stdio_init_all();
 
-    // set up the state manager
-    using namespace STATEMANAGER;
-    auto* pTankSteerStrategy = new MIXER::TankSteerStrategy();
-    auto* pStateManager = new StateManager(pTankSteerStrategy);
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("BNO080 Read Example");
 
-    // set up the receiver
-    // if the cmake build flag RX_PROTOCOL is CPPM, then use the CPPM receiver
-    // otherwise use the SBUS receiver
-    Receiver* pReceiver = getReceiver(motor::motor2040::RX_ECHO);
+  Wire.begin();
 
-    // set up the navigator
-    Navigator navigator = Navigator(pReceiver, pStateManager);
+  if (myIMU.begin() == false)
+  {
+    Serial.println(F("BNO080 not detected at default I2C address. Check your jumpers and the hookup guide. Freezing..."));
+    while (1)
+      ;
+  }
 
-    while (true) {
-        navigator.navigate();
-        sleep_ms(20);
+  Wire.setClock(400000); //Increase I2C data rate to 400kHz
+
+  myIMU.enableRotationVector(50); //Send data update every 50ms
+
+  Serial.println(F("Rotation vector enabled"));
+  Serial.println(F("Output in form roll, pitch, yaw"));
+  while (1){
+    //Look for reports from the IMU
+    if (myIMU.dataAvailable() == true)
+    {
+        float roll = (myIMU.getRoll()) * 180.0 / PI; // Convert roll to degrees
+        float pitch = (myIMU.getPitch()) * 180.0 / PI; // Convert pitch to degrees
+        float yaw = (myIMU.getYaw()) * 180.0 / PI; // Convert yaw / heading to degrees
+
+        Serial.print(roll, 1);
+        Serial.print(F(","));
+        Serial.print(pitch, 1);
+        Serial.print(F(","));
+        Serial.print(yaw, 1);
+
+        Serial.println();
     }
+  }
 }
