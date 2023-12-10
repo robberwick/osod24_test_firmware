846f5eb37f33db0ac7f76334bfe1b24e992f96d5
markmellors <mark.mellors@gmail.com>
2023-12-10 19:37:00 +0000

    commented out prints

--- libs/navigator/src/navigator.cpp
+++ libs/navigator/src/navigator.cpp
@@ -12,17 +12,17 @@ Navigator::Navigator(const Receiver* receiver, STATEMANAGER::StateManager* state
 }
 
 void Navigator::navigate() {
-    printf("Navigating...\n");
+    //printf("Navigating...\n");
     if (receiver->get_receiver_data()) {
-        printf("Receiver data available\n");
+        //printf("Receiver data available\n");
         ReceiverChannelValues values = receiver->get_channel_values();
-        printf("AIL: %f ", values.AIL);
-        printf("ELE: %f ", values.ELE);
-        printf("THR: %f ", values.THR);
-        printf("RUD: %f ", values.RUD);
-        printf("AUX: %f ", values.AUX);
-        printf("NC: %f ", values.NC);
-        printf("\n");
+        //printf("AIL: %f ", values.AIL);
+        //printf("ELE: %f ", values.ELE);
+        //printf("THR: %f ", values.THR);
+        //printf("RUD: %f ", values.RUD);
+        //printf("AUX: %f ", values.AUX);
+        //printf("NC: %f ", values.NC);
+        //printf("\n");
 
         // send the receiver data to the state manager
         // TODO: use a queue to send the receiver data to the state manager
