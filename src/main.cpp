f1bdaf48de3f6bad119439068b755928a1fe7800
markmellors <mark.mellors@gmail.com>
2023-12-10 11:52:33 +0000

    arduino example copied verbatim, no mods yet to work on rp2040

--- src/main.cpp
+++ src/main.cpp
@@ -19,41 +19,41 @@ extern "C" void timer_callback(repeating_timer_t *t) {
 }
 
 int main() {
-    stdio_init_all();
+  stdio_init_all();
 
-    // set up the state estimator
-    auto *pStateEstimator = new STATE_ESTIMATOR::StateEstimator();
+  // set up the state estimator
+  auto *pStateEstimator = new STATE_ESTIMATOR::StateEstimator();
 
-    // set up the state manager
-    using namespace STATEMANAGER;
+  // set up the state manager
+  using namespace STATEMANAGER;
 
-    auto* pAckermannSteerStrategy = new MIXER::AckermannMixer(CONFIG::WHEEL_TRACK, CONFIG::WHEEL_BASE);
-    auto* pStateManager = new StateManager(pAckermannSteerStrategy, pStateEstimator);
+  auto* pAckermannSteerStrategy = new MIXER::AckermannMixer(CONFIG::WHEEL_TRACK, CONFIG::WHEEL_BASE);
+  auto* pStateManager = new StateManager(pAckermannSteerStrategy, pStateEstimator);
 
 
-    // set up the receiver
-    // if the cmake build flag RX_PROTOCOL is CPPM, then use the CPPM receiver
-    // otherwise use the SBUS receiver
-    Receiver *pReceiver = getReceiver(motor::motor2040::RX_ECHO);
+  // set up the receiver
+  // if the cmake build flag RX_PROTOCOL is CPPM, then use the CPPM receiver
+  // otherwise use the SBUS receiver
+  Receiver *pReceiver = getReceiver(motor::motor2040::RX_ECHO);
 
-    // set up the navigator
-    navigator = new Navigator(pReceiver, pStateManager);
+  // set up the navigator
+  navigator = new Navigator(pReceiver, pStateManager);
 
-    // Initialize a hardware timer
-    repeating_timer_t navigationTimer;
-    add_repeating_timer_ms(
-            20,
-            reinterpret_cast<repeating_timer_callback_t>(timer_callback),
-            nullptr,
-            &navigationTimer
-    );
+  // Initialize a hardware timer
+  repeating_timer_t navigationTimer;
+  add_repeating_timer_ms(
+          20,
+          reinterpret_cast<repeating_timer_callback_t>(timer_callback),
+          nullptr,
+          &navigationTimer
+  );
 
-    while (true) {
-        // Do nothing in the main loop
-        if (shouldNavigate) {
-            // Call the navigate function in the interrupt handler
-            navigator->navigate();
-            shouldNavigate = false;
-        }
-    }
+  while (true) {
+      // Do nothing in the main loop
+      if (shouldNavigate) {
+          // Call the navigate function in the interrupt handler
+          navigator->navigate();
+          shouldNavigate = false;
+      }
+  }
 }
