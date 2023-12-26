21493a5decbf0c84cdfd07e6b65a50544fcb3577
markmellors <mark.mellors@gmail.com>
2023-12-26 14:03:35 +0000

    switched to using the SI_units_mixer, fixed the steering sensitivity

--- libs/mixer/src/tank_steer_strategy.cpp
+++ libs/mixer/src/tank_steer_strategy.cpp
@@ -7,18 +7,7 @@
 using namespace MIXER;
 
 MotorSpeeds TankSteerStrategy::mix(float velocity, float angularVelocity) {
-    float left = velocity - angularVelocity;
-    float right = velocity + angularVelocity;
-    float abs_left = left >= 0 ? left : -left;
-    float abs_right = right >= 0 ? right : -right;
-    float max_abs = abs_left > abs_right ? abs_left : abs_right;
-
-    if (max_abs > 1.0) {
-        left /= max_abs;
-        right /= max_abs;
-    }
-    const float speed_factor = 1.0f;
-    float scaled_left = (left * speed_factor);
-    float scaled_right = (right * speed_factor) * -1;
-    return {scaled_left, scaled_right, scaled_left, scaled_right};
+    float left = velocity - angularVelocity * track_width / 2;
+    float right = -velocity - angularVelocity * track_width / 2;
+    return {left, right, left, right};
 }
