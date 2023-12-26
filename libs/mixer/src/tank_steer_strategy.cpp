b6cea3627afaa664794af3ac410544ce10b6552e
markmellors <mark.mellors@gmail.com>
2023-12-26 18:55:57 +0000

    update mixer to account for reversed elevator values, again?!

--- libs/mixer/src/tank_steer_strategy.cpp
+++ libs/mixer/src/tank_steer_strategy.cpp
@@ -7,8 +7,8 @@
 using namespace MIXER;
 
 MotorSpeeds TankSteerStrategy::mix(float velocity, float angularVelocity) {
-    float left = velocity + angularVelocity;
-    float right = velocity - angularVelocity;
+    float left = velocity - angularVelocity;
+    float right = velocity + angularVelocity;
     float abs_left = left >= 0 ? left : -left;
     float abs_right = right >= 0 ? right : -right;
     float max_abs = abs_left > abs_right ? abs_left : abs_right;
@@ -18,7 +18,7 @@ MotorSpeeds TankSteerStrategy::mix(float velocity, float angularVelocity) {
         right /= max_abs;
     }
     const float speed_factor = 1.0f;
-    float scaled_left = (left * -speed_factor);
-    float scaled_right = (right * -speed_factor) * -1;
+    float scaled_left = (left * speed_factor);
+    float scaled_right = (right * speed_factor) * -1;
     return {scaled_left, scaled_right, scaled_left, scaled_right};
 }
