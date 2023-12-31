9c47b4c941a62becac2faa5f194a9d9818c62d0e
Rob Berwick <rob.berwick@gmail.com>
2023-12-31 11:12:26 +0000

    whitespace changes

--- libs/config/include/drivetrain_config.h
+++ libs/config/include/drivetrain_config.h
@@ -13,9 +13,10 @@ namespace CONFIG {
         PI_NOON,
         TEMPLE_OF_DOOM
     };
-// define CURRENT_CHALLENGE in a earlier file with:
-// Challenge CURRENT_CHALLENGE = LAVA_PALAVA;
-// To set the current challenge and the drivetrain config appropriately
+
+    // define CURRENT_CHALLENGE in a earlier file with:
+    // Challenge CURRENT_CHALLENGE = LAVA_PALAVA;
+    // To set the current challenge and the drivetrain config appropriately
 
     enum Handedness {
         LEFT,
@@ -31,19 +32,19 @@ namespace CONFIG {
     constexpr float MAX_STEERING_ANGLE = 3.14 / 4; // radians
     const float STEERING_HYPOTENUSE = std::sqrt(HALF_WHEEL_TRACK * HALF_WHEEL_TRACK + WHEEL_BASE * WHEEL_BASE);
 
-//left steering servo
+    //left steering servo
     static constexpr float LEFT_MIN_PULSE = 1032.0f; // usec
     static constexpr float LEFT_MID_PULSE = 1599.0f; // usec
     static constexpr float LEFT_MAX_PULSE = 2200.0f; // usec
-    static constexpr float LEFT_MID_VALUE = 0.0f;    // radians @LEFT_MID_PULSE
-    static constexpr float LEFT_MIN_VALUE = 3.14 / 4;  // radians  @LEFT_MIN_PULSE (pi/4radians = 45degrees)
+    static constexpr float LEFT_MID_VALUE = 0.0f; // radians @LEFT_MID_PULSE
+    static constexpr float LEFT_MIN_VALUE = 3.14 / 4; // radians  @LEFT_MIN_PULSE (pi/4radians = 45degrees)
 
-//right steering servo
+    //right steering servo
     static constexpr float RIGHT_MIN_PULSE = 1221.0f; // usec
     static constexpr float RIGHT_MID_PULSE = 1670.0f; // usec
     static constexpr float RIGHT_MAX_PULSE = 2200.0f; // usec
-    static constexpr float RIGHT_MID_VALUE = 0.0f;    // radians @RIGHT_MID_PULSE
-    static constexpr float RIGHT_MIN_VALUE = 3.14 / 4;  // radians  @RIGHT_MIN_PULSE (pi/4radians = 45degrees)
+    static constexpr float RIGHT_MID_VALUE = 0.0f; // radians @RIGHT_MID_PULSE
+    static constexpr float RIGHT_MIN_VALUE = 3.14 / 4; // radians  @RIGHT_MIN_PULSE (pi/4radians = 45degrees)
 
 
     //wheels and gearing
@@ -53,7 +54,7 @@ namespace CONFIG {
     constexpr float GEARMOTOR_RATIO = 19.22f; // -to-1
 
 
-// Define WHEEL_DIAMETER and GEAR_RATIO based on CURRENT_CHALLENGE
+    // Define WHEEL_DIAMETER and GEAR_RATIO based on CURRENT_CHALLENGE
 #if CURRENT_CHALLENGE == ECO_DISASTER
     constexpr float WHEEL_DIAMETER = LARGE_WHEEL_DIAMETER;
     constexpr float GEAR_RATIO = 42.0 / 18.0 * GEARMOTOR_RATIO;
@@ -74,8 +75,8 @@ namespace CONFIG {
 #endif
 
 
-// motor properties
-// The counts per rev of the motor
+    // motor properties
+    // The counts per rev of the motor
     constexpr int CPR = 12;
     // The counts per revolution of the wheel
     // note that this is not constexpr because it depends on the gear ratio which is not constexpr
@@ -91,14 +92,14 @@ namespace CONFIG {
     constexpr float MAX_ANGULAR_ACCELERATION = 20; // rad/s^2
 
 
-// Control constants such as PID & feedforward
-// PID values
-    constexpr float VEL_KP = 3.25f;   // Velocity proportional (P) gain
-    constexpr float VEL_KI = 0.0f;    // Velocity integral (I) gain
-    constexpr float VEL_KD = 0.003f;    // Velocity derivative (D) gain
+    // Control constants such as PID & feedforward
+    // PID values
+    constexpr float VEL_KP = 3.25f; // Velocity proportional (P) gain
+    constexpr float VEL_KI = 0.0f; // Velocity integral (I) gain
+    constexpr float VEL_KD = 0.003f; // Velocity derivative (D) gain
 
-// feedforward values
-    constexpr float VEL_FF_GAIN = 1.0f;   // Velocity feedforward gain
-    constexpr float ACC_FF_GAIN = 0.0f;    // Acceleration feedforward gain
+    // feedforward values
+    constexpr float VEL_FF_GAIN = 1.0f; // Velocity feedforward gain
+    constexpr float ACC_FF_GAIN = 0.0f; // Acceleration feedforward gain
 }
 #endif // DRIVETRAIN_CONFIG_H
