ee293e2f192a87cd611bebf7df6a1e3063de3a40
markmellors <mark.mellors@gmail.com>
2024-01-09 10:06:35 +0000

    added PID speed control, working

--- libs/stoker/include/stoker.h
+++ libs/stoker/include/stoker.h
@@ -7,6 +7,8 @@
 
 #include "interfaces.h"
 #include "drivers/motor/motor.hpp"
+#include "drivetrain_config.h"
+#include "pid.hpp"
 
 namespace STOKER {
     using namespace COMMON;
@@ -24,6 +26,10 @@ namespace STOKER {
         motor::Motor motor;
         float current_motor_speed = 0.0f;
         MOTOR_POSITION::MotorPosition motor_position_;
+        
+        // UPDATE_RATE a fixed number for now, should really be passed in at initialisation
+        float UPDATE_RATE = 0.02; //seconds
+        PID vel_pid = PID(CONFIG::VEL_KP, CONFIG::VEL_KI, CONFIG::VEL_KD, UPDATE_RATE);
     };
 
 } // STOKER
