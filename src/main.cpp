8a46438c1f945e2be63b03d9a8f81b2e1e89630c
markmellors <mark.mellors@gmail.com>
2024-02-19 17:08:39 +0000

    modifications to start integrating the ssd1305 arduino library

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,20 @@
 #include "tank_steer_strategy.h"
 #include "ackermann_strategy.h"
 #include "drivetrain_config.h"
+#include "hardware/spi.h"
+#include "ssd1305.h"
+
+#define WIDTH     128
+#define HEIGHT     32
+#define PAGES       4
+
+#define OLED_RST    9 
+#define OLED_DC     8
+#define OLED_CS    10
+#define SPI_MOSI   11    /* connect to the DIN pin of OLED */
+#define SPI_SCK    13     /* connect to the CLK pin of OLED */
+
+uint8_t oled_buf[WIDTH * HEIGHT / 8];
 
 Navigator *navigator;
 bool shouldNavigate = false;
@@ -29,7 +43,9 @@ int main() {
 
     auto* pAckermannSteerStrategy = new MIXER::AckermannMixer(CONFIG::WHEEL_TRACK, CONFIG::WHEEL_BASE);
     auto* pStateManager = new StateManager(pAckermannSteerStrategy, pStateEstimator);
-
+    SSD1305_begin();
+    SSD1305_clear(oled_buf);
+    SSD1305_string(52, 52, "MENU", 12, 0, oled_buf); 
 
     // set up the receiver
     // if the cmake build flag RX_PROTOCOL is CPPM, then use the CPPM receiver
