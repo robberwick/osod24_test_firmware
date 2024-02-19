17db34dddc6821637dadfd7ec9bf1e9d25bb009b
markmellors <mark.mellors@gmail.com>
2024-02-19 11:25:30 +0000

    changed getLidarData to accept an I2C port, added reading all sensors, working

--- libs/tf_luna/src/tf_luna.cpp
+++ libs/tf_luna/src/tf_luna.cpp
@@ -3,13 +3,13 @@
 #include "tf_luna.h"
 
 // Function to get Lidar data
-LidarData getLidarData(uint8_t i2c_addr) {
+LidarData getLidarData(uint8_t i2c_addr, i2c_inst_t* i2c_port) {
     uint8_t temp[9] = {0};
-    i2c_write_blocking(i2c1, i2c_addr, getLidarDataCmd, 5, false); // Send command
-    i2c_read_blocking(i2c1, i2c_addr, temp, 9, false); // Read response
+    i2c_write_blocking(i2c_port, i2c_addr, getLidarDataCmd, 5, false); // Send command
+    i2c_read_blocking(i2c_port, i2c_addr, temp, 9, false); // Read response
 
     LidarData data = {0, 0, 0}; // Initialize to zero
-
+    
     if (temp[0] == 0x59 && temp[1] == 0x59) {
         data.distance = temp[2] + temp[3] * 256; // Distance value
         data.strength = temp[4] + temp[5] * 256; // Signal strength
