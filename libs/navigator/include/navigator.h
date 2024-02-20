7f436f1d3f5868de038b9b295d718512899fb0d4
markmellors <mark.mellors@gmail.com>
2024-02-20 21:29:37 +0000

    added a navigation mode, chosen by the rx's throttle channel

--- libs/navigator/include/navigator.h
+++ libs/navigator/include/navigator.h
@@ -1,6 +1,7 @@
 #include "receiver.h"
 #include "statemanager.h"
 #include "drivetrain_config.h"
+#include "types.h"
 
 class Navigator {
 public:
@@ -8,8 +9,11 @@ public:
     ~Navigator();
     void navigate();
     CONFIG::SteeringStyle driveDirection; //factor to change requested motor speed direction based on what we currently consider the front
+    COMMON::NavigationMode navigationMode;
 
 private:
     const Receiver *receiver{};
     STATEMANAGER::StateManager *pStateManager;
+    float waypointSignalThreshold = 0.5; //if signal above this, we're move into waypoint mode
+    COMMON::NavigationMode determineMode(float signal);
 };
--- libs/navigator/src/navigator.cpp
+++ libs/navigator/src/navigator.cpp
@@ -2,6 +2,7 @@
 #include "navigator.h"
 #include "statemanager.h"
 #include "state_estimator.h"
+#include "types.h"
 #include "drivetrain_config.h"
 
 Navigator::Navigator(const Receiver* receiver, STATEMANAGER::StateManager* stateManager, CONFIG::SteeringStyle direction) {
@@ -9,6 +10,7 @@ Navigator::Navigator(const Receiver* receiver, STATEMANAGER::StateManager* state
     this->pStateManager = stateManager;
 
     driveDirection = direction;
+    navigationMode = COMMON::REMOTE_CONTROL;
 }
 
 void Navigator::navigate() {
@@ -16,23 +18,45 @@ void Navigator::navigate() {
     if (receiver->get_receiver_data()) {
         //printf("Receiver data available\n");
         ReceiverChannelValues values = receiver->get_channel_values();
-        //printf("AIL: %f ", values.AIL);
-        //printf("ELE: %f ", values.ELE);
-        //printf("THR: %f ", values.THR);
-        //printf("RUD: %f ", values.RUD);
-        //printf("AUX: %f ", values.AUX);
-        //printf("NC: %f ", values.NC);
-        //printf("\n");
 
+        COMMON::NavigationMode newMode;
+        newMode = determineMode(values.THR);
+        if (newMode != navigationMode){
+            printf("changing mode to mode %d, where 1=RC, 2=waypoint, 3=Pi\n", newMode);
+            navigationMode = newMode;
+        }
+        STATE_ESTIMATOR::State requestedState{};
+
+        switch (navigationMode) {
+        case COMMON::REMOTE_CONTROL:
+            requestedState.velocity.velocity = values.ELE * CONFIG::MAX_VELOCITY;
+            requestedState.velocity.angular_velocity = values.AIL * CONFIG::MAX_ANGULAR_VELOCITY;
+            break;
+        case COMMON::WAYPOINT:
+            requestedState.velocity.velocity = 0;
+            requestedState.velocity.angular_velocity = 0;
+            break;
+        default:
+            requestedState.velocity.velocity = values.ELE * CONFIG::MAX_VELOCITY;
+            requestedState.velocity.angular_velocity = values.AIL * CONFIG::MAX_ANGULAR_VELOCITY;
+            break;
+        }
         // send the receiver data to the state manager
         // TODO: use a queue to send the receiver data to the state manager
-        STATE_ESTIMATOR::State requestedState{};
-        requestedState.velocity.velocity = driveDirection * values.ELE * CONFIG::MAX_VELOCITY;
-        requestedState.velocity.angular_velocity = values.AIL * CONFIG::MAX_ANGULAR_VELOCITY;
         pStateManager->requestState(requestedState);
     } else {
         printf("No receiver data available\n");
     }
+}
+
+COMMON::NavigationMode Navigator::determineMode(float signal){
+    COMMON::NavigationMode mode;
+    if (signal > waypointSignalThreshold){
+        mode = COMMON::WAYPOINT;
+    } else {
+        mode = COMMON::REMOTE_CONTROL;
+    }
+    return mode;
 }
 
 Navigator::~Navigator() = default;
