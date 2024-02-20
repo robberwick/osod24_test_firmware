a36a38542e123ae42a8977fcc8bfddf73e9d69c5
markmellors <mark.mellors@gmail.com>
2024-02-20 21:29:37 +0000

    added a navigation mode, chosen by the rx's throttle channel

--- libs/navigator/include/navigator.h
+++ libs/navigator/include/navigator.h
@@ -1,13 +1,17 @@
 #include "receiver.h"
 #include "statemanager.h"
+#include "types.h"
 
 class Navigator {
 public:
     explicit Navigator(const Receiver* receiver, STATEMANAGER::StateManager *stateManager);
     ~Navigator();
     void navigate();
+    COMMON::NavigationMode navigationMode;
 
 private:
     const Receiver *receiver{};
     STATEMANAGER::StateManager *pStateManager;
+    float waypointSignalThreshold = 0.5; //if signal above this, we're move into waypoint mode
+    COMMON::NavigationMode determineMode(float signal);
 };
