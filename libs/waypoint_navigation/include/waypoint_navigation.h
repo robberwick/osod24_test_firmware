de38d36e702e740f24d389d2ce51aceb2fc748a1
markmellors <mark.mellors@gmail.com>
2024-02-22 17:35:03 +0000

    fleshed out methods, should be complete but untested

--- libs/waypoint_navigation/include/waypoint_navigation.h
+++ libs/waypoint_navigation/include/waypoint_navigation.h
@@ -1,13 +1,46 @@
+
+#ifndef WAYPOINT_NAVIGATION_H
+#define WAYPOINT_NAVIGATION_H
+
 #include "types.h"
 #include "drivetrain_config.h"
+#include "utils.h"
+#include "pid.hpp"
 
-using namespace COMMON;
-class WaypointNavigation {
-public:
-    ~WaypointNavigation();
-    void navigate(State currentState);
+namespace WAYPOINTS {
+    using namespace COMMON;
 
-private:
-    float desiredV;  // desired velocity to get to next waypoint
-    float desiredW;  // desired angular velocity to get to next waypoint
-};
+    class WaypointNavigation {
+    public:
+        WaypointNavigation();
+        ~WaypointNavigation();
+        void navigate(const State currentState); //update the desired movement to get to the next waypoint
+        float desiredV;  // desired velocity to get to next waypoint
+        float desiredW;  // desired angular velocity to get to next waypoint
+        const static uint8_t waypointBufferSize = 40;
+        Waypoint waypointBuffer[waypointBufferSize]; // waypoint buffer
+        void addWaypoint(const Waypoint newWaypoint); // add a waypoint to the buffer
+        bool isWaypointEmpty(const Waypoint& waypoint); // check if a waypoint slot in the buffer is empty
+        uint8_t targetWaypointIndex; // the index of the current waypoint we're navigating to
+        Waypoint targetWaypoint; // the actual waypoint we're navigating to
+        uint8_t nextWaypoint(const uint8_t currentWaypointIndex,const State currentState); //find the index of the next waypoint to navigate to
+        uint8_t nearestWaypointIndex; // the index of the waypoint we're closest to
+        uint8_t nearestWaypoint(const State currentState); //find the index of the closest waypoint
+        void clearWaypointBuffer(); // clear all waypoints from buffer
+        float headingToWaypoint(const Waypoint target,const  State currentState); // heading to a waypoint. not currently used?
+        float bearingToWaypoint(const Waypoint target, const State currentState); // Compass bearing to a waypoint
+        float distanceToWaypoint(const Waypoint target, const State currentState); // distance to a waypoint
+        float lookAhead = 0.2; //lookahead distance in metres
+        float maxTurnVelocity = 1; //max turn velocity in radians per second
+        float unwrapHeading(const float targetHeading, float currentHeading); 
+
+    private:
+        float headingPGain = 1;
+        float headingIGain = 0;
+        float headingDGain = 0;
+        float UPDATE_RATE = 0.02; //seconds
+        using namespace pimoroni;
+        PID headingPID = PID(headingPGain, headingIGain, headingDGain, UPDATE_RATE); // used for steering to waypoints
+    };
+}
+#endif // WAYPOINT_NAVIGATION_H
