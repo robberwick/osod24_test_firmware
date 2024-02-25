e813bc242ff4dda7d8e32bb2b672584f8d6babbb
markmellors <mark.mellors@gmail.com>
2024-02-25 17:04:30 +0000

    wrap_pi made more readable with an explicit if-else structure

--- libs/common/src/utils.cpp
+++ libs/common/src/utils.cpp
@@ -2,8 +2,15 @@
 #include "utils.h"
 
 float wrap_pi(const float heading) {
-    //constrain heading to +/-pi
+    // constrain heading to within +/-pi (+/-180 degrees) without changing the meaning of the angle
+    // if its more than pi (+180), subtract 2*pi (subtract 360degrees) so we have the "smaller" angle 
+    float wrapped = heading;
 
-    const double wrapped = heading > M_PI ? heading - M_TWOPI : heading < -M_TWOPI ? heading + M_TWOPI : heading;
+    if (heading > M_PI) {
+        wrapped = heading - M_TWOPI;
+    } else if (heading < -M_PI) {
+        wrapped = heading + M_TWOPI;
+    }
+
     return static_cast<float>(wrapped);
 }
