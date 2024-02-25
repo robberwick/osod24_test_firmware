8ab9396a958d9724babdd913df0df185a450fc0d
markmellors <mark.mellors@gmail.com>
2024-02-25 17:04:30 +0000

    wrap_pi made more readable with an explicit if-else structure

--- libs/common/src/utils.cpp
+++ libs/common/src/utils.cpp
@@ -14,8 +14,15 @@ void initI2C(i2c_inst_t* &i2c_port, uint baudrate, uint sda_pin, uint scl_pin) {
 }
 
 float wrap_pi(const float heading) {
-    //constrain heading to +/-pi
+    // constrain heading to within +/-pi (+/-180 degrees) without changing the meaning of the angle
+    // if its more than pi (+180), subtract 2*pi (subtract 360degrees) so we have the "smaller" angle 
+    float wrapped = heading;
 
-    const double wrapped = heading > M_PI ? heading - M_TWOPI : heading < -M_TWOPI ? heading + M_TWOPI : heading;
+    if (heading > M_PI) {
+        wrapped = heading - M_TWOPI;
+    } else if (heading < -M_PI) {
+        wrapped = heading + M_TWOPI;
+    }
+
     return static_cast<float>(wrapped);
 }
