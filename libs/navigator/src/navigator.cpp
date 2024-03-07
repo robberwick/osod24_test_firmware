5fe76bb52a9a4ad49cae0dd829704c9f65186049
markmellors <mark.mellors@gmail.com>
2024-03-07 21:00:35 +0000

    added methods to set odometry heading and origin, and triggered them with key transmitter signals

--- libs/navigator/src/navigator.cpp
+++ libs/navigator/src/navigator.cpp
@@ -37,9 +37,44 @@ void Navigator::navigate() {
     }
 }
 
+bool Navigator::shouldSetHeading(float signal){
+    return (signal < setHeadingThreshold);
+}
+
+bool Navigator::shouldSetOdometryOrigin(float signal){
+    return (signal > setOriginThreshold);
+}
+
+void Navigator::setHeading(){
+    //#TODO: add a timeout blocker to prevent this function being called twice in quick succession?
+    pStateEstimator->set_heading_offset();
+}
+
+void Navigator::setOrigin(){
+    //As above: #TODO: add a timeout blocker to prevent this function being called twice in quick succession
+    // as that undos the original reset, if its called before the values have propogated through
+    pStateEstimator->apply_odometry_offset(current_state.odometry.x, current_state.odometry.y);
+}
 
 void Navigator::update(const COMMON::VehicleState newState) {
     current_state = newState;
+}
+
+void Navigator::parseTxSignals(ReceiverChannelValues signals){
+    // function to use "spare" transmitter channels as auxiliary inputs
+    // currently can set (zero) odoemtry heading and and origin
+        if (shouldSetHeading(signals.RUD)){
+            printf("setting current heading to 0.\n");
+            setHeading();
+            //a time delay seems to be needed after setting the heading to allow
+            //the signals to propogate through before this function can be called again.
+            sleep_ms(100);
+        }
+        if (shouldSetOdometryOrigin(signals.RUD)){
+            printf("setting current position as zero for odometry.\n");
+            setOrigin();
+            sleep_ms(100);
+        }
 }
 
 Navigator::~Navigator() = default;
