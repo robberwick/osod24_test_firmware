22a4fe858c237583fafff34c5febc691926dbb70
markmellors <mark.mellors@gmail.com>
2024-03-07 16:47:52 +0000

    more PID tuning

--- libs/waypoint_navigation/include/waypoint_navigation.h
+++ libs/waypoint_navigation/include/waypoint_navigation.h
@@ -17,27 +17,27 @@ namespace WAYPOINTS {
         void navigate(const VehicleState& currentState); //update the desired movement to get to the next waypoint
         float desiredV;  // desired velocity to get to next waypoint
         float desiredW;  // desired angular velocity to get to next waypoint
-        const static uint8_t waypointBufferSize = 40;
+        const static uint8_t waypointBufferSize = 74;
         Waypoint waypointBuffer[waypointBufferSize]; // waypoint buffer
         void addWaypoint(const Waypoint& newWaypoint); // add a waypoint to the buffer
         bool isWaypointEmpty(const Waypoint& waypoint); // check if a waypoint slot in the buffer is empty
         uint8_t targetWaypointIndex = 0; // the index of the current waypoint we're navigating to
         Waypoint targetWaypoint; // the actual waypoint we're navigating to
         uint8_t nextWaypoint(const uint8_t currentWaypointIndex, const VehicleState& currentState); //find the index of the next waypoint to navigate to
-        uint8_t nearestWaypointIndex; // the index of the waypoint we're closest to
+        uint8_t nearestWaypointIndex = 0; // the index of the waypoint we're closest to
         uint8_t nearestWaypoint(const VehicleState& currentState); //find the index of the closest waypoint
         void clearWaypointBuffer(); // clear all waypoints from buffer
         float headingToWaypoint(const Waypoint& target, const VehicleState& currentState); // heading to a waypoint. not currently used?
         float bearingToWaypoint(const Waypoint& target, const VehicleState& currentState); // Compass bearing to a waypoint
         float distanceToWaypoint(const Waypoint& target, const VehicleState& currentState); // distance to a waypoint
         float lookAhead = 0.2; //lookahead distance in metres
-        float maxTurnVelocity = 1; //max turn velocity in radians per second
+        float maxTurnVelocity = 2.0; //max turn velocity in radians per second
         float unwrapHeading(const float targetHeading, float currentHeading); //find "nearest" description of current heading to target
 
     private:
-        float headingPGain = 6;
-        float headingIGain = 0;
-        float headingDGain = 6;
+        float headingPGain = 20.0;
+        float headingIGain = 0.5;
+        float headingDGain = 5.0;
         float UPDATE_RATE = 0.02; //seconds
         Waypoint nanWaypoint = {NAN, NAN, NAN, NAN}; // Create a NaN waypoint
 
