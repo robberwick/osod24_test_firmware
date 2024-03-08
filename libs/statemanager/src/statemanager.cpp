76ac9faa69a1c8b34d31fc424f7e3015e04f3bb2
markmellors <mark.mellors@gmail.com>
2024-03-08 21:44:23 +0000

    servo calibration corrected for latest chassis build

--- libs/statemanager/src/statemanager.cpp
+++ libs/statemanager/src/statemanager.cpp
@@ -29,9 +29,9 @@ namespace STATEMANAGER {
 
         // set up the servos
         // left - ADC2 / PWM 6 - Pin 28
-        initialiseServo(steering_servos.left, motor2040::ADC2, 2200, 1599, 1032);
+        initialiseServo(steering_servos.left, motor2040::ADC2, 1221, 1750, 2200);
         // right - TX_TRIG / PWM 0 - Pin 16
-        initialiseServo(steering_servos.right, motor2040::TX_TRIG, 1221, 1670, 2200);
+        initialiseServo(steering_servos.right, motor2040::TX_TRIG, 2000, 1400, 830);
     }
 
     void StateManager::requestState(const STATE_ESTIMATOR::State& requestedState) {
