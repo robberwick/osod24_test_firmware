8a663c5f38f846413bb95563c042aa49aea36752
markmellors <mark.mellors@gmail.com>
2024-03-10 20:44:19 +0000

    added the #include <array> needed to use std::array<>

--- libs/tf_luna/src/tf_luna.cpp
+++ libs/tf_luna/src/tf_luna.cpp
@@ -1,22 +1,9 @@
 #include "pico/stdlib.h"
+#include <array>
 #include "hardware/i2c.h"
 #include "tf_luna.h"
 
 // Function to get Lidar data
-LidarData getLidarData(uint8_t i2c_addr, i2c_inst_t* i2c_port) {
-    uint8_t temp[9] = {0};
-    i2c_write_blocking(i2c_port, i2c_addr, getLidarDataCmd, 5, false); // Send command
-    i2c_read_blocking(i2c_port, i2c_addr, temp, 9, false); // Read response
-
-    LidarData data = {0, 0, 0}; // Initialize to zero
-    
-    if (temp[0] == 0x59 && temp[1] == 0x59) {
-        data.distance = temp[2] + temp[3] * 256; // Distance value
-        data.strength = temp[4] + temp[5] * 256; // Signal strength
-        data.temperature = (temp[6] + temp[7] * 256) / 8 - 256; // Chip temperature
-    }
-
-    return data;
 LidarData getLidarData(uint8_t i2c_addr, i2c_inst_t* i2c_port) {
     std::array<uint8_t, 9> temp = {0};
     i2c_write_blocking(i2c_port, i2c_addr, getLidarDataCmd, 5, false); // Send command
