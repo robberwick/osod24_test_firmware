9ab3c1440f39799a5e6adf653f04b860cd98c17a
markmellors <mark.mellors@gmail.com>
2024-03-10 21:06:08 +0000

    renamed old getLidarData to GetSingleLidarData to better differentiate it from getAllLidarDistances

--- libs/tf_luna/src/tf_luna.cpp
+++ libs/tf_luna/src/tf_luna.cpp
@@ -3,8 +3,8 @@
 #include "hardware/i2c.h"
 #include "tf_luna.h"
 
-// Function to get Lidar data
-LidarData getLidarData(uint8_t i2c_addr, i2c_inst_t* i2c_port) {
+// Function to get a single Lidar's data
+LidarData getSingleLidarData(uint8_t i2c_addr, i2c_inst_t* i2c_port) {
     std::array<uint8_t, 9> temp = {0};
     i2c_write_blocking(i2c_port, i2c_addr, getLidarDataCmd, 5, false); // Send command
     i2c_read_blocking(i2c_port, i2c_addr, temp.data(), 9, false); // Read response
@@ -25,10 +25,10 @@ LidarData getLidarData(uint8_t i2c_addr, i2c_inst_t* i2c_port) {
 FourTofDistances getAllLidarDistances(i2c_inst_t* i2c_port) {
     // function to get the distances of four ToF sensors
     // Get distance from each sensor
-    int frontDistance = getLidarData(tf_luna_front, i2c_port).distance;
-    int rightDistance = getLidarData(tf_luna_right, i2c_port).distance;
-    int rearDistance = getLidarData(tf_luna_rear, i2c_port).distance;
-    int leftDistance = getLidarData(tf_luna_left, i2c_port).distance;
+    int frontDistance = getSingleLidarData(tf_luna_front, i2c_port).distance;
+    int rightDistance = getSingleLidarData(tf_luna_right, i2c_port).distance;
+    int rearDistance = getSingleLidarData(tf_luna_rear, i2c_port).distance;
+    int leftDistance = getSingleLidarData(tf_luna_left, i2c_port).distance;
 
     // Return the struct populated with the distances
     return {frontDistance, rightDistance, rearDistance, leftDistance};
