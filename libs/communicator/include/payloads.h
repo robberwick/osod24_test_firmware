3a30eb03b00edaf0fcf250f6dd26a534be9246a5
Rob Berwick <rob.berwick@gmail.com>
2024-04-01 13:17:38 +0100

    add namespace for packet type

--- libs/communicator/include/payloads.h
+++ libs/communicator/include/payloads.h
@@ -3,11 +3,19 @@
 #include "types.h"
 
 namespace PAYLOADS {
+    namespace PACKET_TYPE {
+        enum PacketType: uint8_t {
+            SERIAL_TRANSFER_AVAILABLE_STATUS = 0x01,
+            REQUESTED_STATE = 0x02,
+            ESTIMATED_STATE = 0x03,
+        };
+    }
+
     // Base class for all payloads
     struct __attribute__((packed)) Payload {
         uint8_t packetType;
 
-        explicit Payload(const uint8_t type) : packetType(type) {
+        explicit Payload(const PACKET_TYPE::PacketType type) : packetType(static_cast<uint8_t>(type)) {
         }
     };
 
@@ -15,7 +23,7 @@ namespace PAYLOADS {
     struct __attribute__((packed)) SerialTransferAvailableStatus : Payload {
         uint8_t available;
 
-        explicit SerialTransferAvailableStatus(const bool available = false) : Payload(0x01), available(available) {
+        explicit SerialTransferAvailableStatus(const bool available = false) : Payload(PACKET_TYPE::SERIAL_TRANSFER_AVAILABLE_STATUS), available(available) {
         }
     };
 
@@ -24,7 +32,7 @@ namespace PAYLOADS {
         float velocity;
         float angular_velocity;
 
-        explicit StatePayload(const float velocity = 0.0f, const float angular_velocity = 0.0f) : Payload(0x01),
+        explicit StatePayload(const float velocity = 0.0f, const float angular_velocity = 0.0f) : Payload(PACKET_TYPE::REQUESTED_STATE),
             velocity(velocity),
             angular_velocity(angular_velocity) {
         }
@@ -45,7 +53,7 @@ namespace PAYLOADS {
             const uint32_t timestamp,
             const COMMON::Pose& odometry,
             const COMMON::FourToFDistances& tofDistances)
-            : Payload(0x03),
+            : Payload(PACKET_TYPE::ESTIMATED_STATE),
               timestamp(timestamp),
               x(odometry.x), y(odometry.y), heading(odometry.heading),
               tofFront(tofDistances.front),
